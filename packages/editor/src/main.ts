b4a1c8801131483326f329eea86afc30591e9775
Marcis Bergmanis <marcisbergmanis@gmail.com>
2023-09-26 12:43:41 +0300

    Update main.ts

--- packages/editor/src/main.ts
+++ packages/editor/src/main.ts
@@ -4,6 +4,7 @@ import {
   recommendedPlugins,
 } from './kode';
 import { statsPlugin } from './plugins/stats';
+import { AtomOneDark } from './themes/atom-one-dark';
 import './style.css';
 
 const editor = document.querySelector<HTMLDivElement>('#editor')!;
@@ -17,13 +18,15 @@ const editor = document.querySelector<HTMLDivElement>('#editor')!;
 //   public inputPlugins: InputPlugin[] = [];
 // }`).join('\n\n');
 
+document.body.style.backgroundColor = AtomOneDark.bg;
+
 const model1 = new Model();
 
 const fontSize = 14;
 const lineHeight = 1.6;
 const EditorInstance1 = new Editor(
   model1,
-  undefined,
+  AtomOneDark,
   [
     ...recommendedPlugins,
     ...(
@@ -42,24 +45,24 @@ const EditorInstance1 = new Editor(
 EditorInstance1.mount(editor);
 
 
-const editor2 = document.querySelector<HTMLDivElement>('#editor2')!;
-const model2 = new Model();
-const EditorInstance2 = new Editor(
-  model2,
-  undefined,
-  [
-    ...recommendedPlugins,
-    ...(
-      process.env.NODE_ENV === 'production'
-        ? [statsPlugin()]
-        : []
-    ),
-  ],
-  {
-    size: fontSize,
-    family: '"Menlo", monospace',
-    lineHeight: fontSize * lineHeight,
-  },
-);
+// const editor2 = document.querySelector<HTMLDivElement>('#editor2')!;
+// const model2 = new Model();
+// const EditorInstance2 = new Editor(
+//   model2,
+//   undefined,
+//   [
+//     ...recommendedPlugins,
+//     ...(
+//       process.env.NODE_ENV === 'production'
+//         ? [statsPlugin()]
+//         : []
+//     ),
+//   ],
+//   {
+//     size: fontSize,
+//     family: '"Menlo", monospace',
+//     lineHeight: fontSize * lineHeight,
+//   },
+// );
 
-EditorInstance2.mount(editor2);
+// EditorInstance2.mount(editor2);
